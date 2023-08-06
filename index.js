4c0fbe43f82f34e397a5f37f8d369ec092a695be
JayFoxall <46763560+JayFoxall@users.noreply.github.com>
2023-08-06 21:17:26 +0100

    All tests pass

--- index.js
+++ index.js
@@ -10,46 +10,112 @@ const DisplayGraph = () => {
     .attr("width", w + 100)
     .attr("height", h + 50);
 
+    var tooltip = d3
+    .select(".graph")
+    .append("div")
+    .attr("id", "tooltip")
+    .style("opacity", 0)
+    .style("position", "absolute");
+
+    var legend = d3
+    .select(".graph")
+    .append("div")
+    .attr("id", "legend")
+    .text("I am legend")
+
+
   document.addEventListener("DOMContentLoaded", () =>
     fetch(
       "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
 
-        let times = data.map((entry) => 
-          entry["Time"]
-        );
-
+        
+        let dopingEntries = data.map(entry => entry)
+        let times = data.map((entry) => entry["Time"]);
+        let seconds = data.map((entry) => entry["Seconds"]);
         let years = data.map((entry) => entry["Year"]);
 
-        const minTime = d3.min(times)
-        const maxTime = d3.max(times)
-        const minYear = d3.min(years)
-        const maxYear = d3.max(years)
+        const minSeconds = d3.min(seconds);
+        const maxSeconds = d3.max(seconds);
+        const minYear = d3.min(years);
+        const maxYear = d3.max(years);
 
-        console.log(minTime, maxTime, minYear, maxYear);
+        const xScale = d3
+          .scaleLinear()
+          .domain([(minYear - 1), maxYear])
+          .range([padding, w - padding]);
 
-        const xScale = d3.scaleLinear().domain([minYear, maxYear]).range([padding,w-padding])
+        const yScale = d3
+          .scaleLinear()
+          .domain([minSeconds, maxSeconds])
+          .range([0, h]);
 
-        const yScale = d3.scaleTime().domain([minTime, maxTime]).range([h,0])
+        const xAxis = d3.axisBottom(xScale)
+                        .tickFormat(d3.format(" "))
+        const yAxis = d3
+                        .axisLeft(yScale)
+                        .tickFormat(function(d){
+                        var minutes = Math.floor(d / 60);
+                        var seconds = (d % 60).toFixed(0);
+                        return minutes + ":" + (seconds === "0" ? "00" : seconds)
+                        })
+                        //.tickFormat(d3.format("g0"))
+                        
+        ;
 
-        const xAxis = d3.axisBottom(xScale);
-
-        const yAxis = d3.axisLeft(yScale);
-        
         svg
-        .append("g")
-        .attr("transform", "translate(0," + (h) + ")")
-        .attr("id", "x-axis")
-        .call(xAxis)
+          .append("g")
+          .attr("transform", "translate(0," + h + ")")
+          .attr("id", "x-axis")
+          .call(xAxis)
 
-      svg
-        .append("g")
-        .attr("transform", "translate(" + padding + ",0)")
-        .attr("id", "y-axis")
-        .call(yAxis);
+        svg
+          .append("g")
+          .attr("transform", "translate(" + padding + ",0)")
+          .attr("id", "y-axis")
+          .call(yAxis);
+
+          svg
+          .selectAll("circle")
+          .data(dopingEntries)
+          .enter()
+          .append("circle")
+          .attr("class", "dot")
+          .attr("data-xvalue", (d) => parseInt(d["Year"]))
+          .attr("data-yvalue", (d) => {
+            //should be split off into a function, which is then reused to make ticks too
+            let mmssArray = d["Time"].split(":")
+            let mmssFormat = new Date()
+            
+            mmssFormat.setMinutes(mmssArray[0]);
+            mmssFormat.setSeconds(mmssArray[1]);
+            return mmssFormat
+          })
+          .attr("cx", (d) => xScale(d["Year"]))
+          .attr("cy", (d) => yScale(d["Seconds"]))
+          .attr("r", 5)
+          .on("mouseover", function (event, d) {;
+            let coordinates = d3.pointer(event)
+            let coordinateY = coordinates[0]
+            let coordinateX = coordinates[1]
+            tooltip.style("opacity", 0.6);
+            tooltip
+              .text(`Name: ${d.Name}, Nationality: ${d.Nationality}
+              Year: ${d.Year}   
+              Time: ${d.Time}     
+              Doping: ${d.Doping}`
+              )
+              .attr("data-year", parseInt(d["Year"]))
+              //.html("your text here")
+              .style('left', (coordinateY) + 'px')
+              .style('top', (coordinateX) + 'px')
+
+          })
+          .on("mouseout", function () {
+            tooltip.style("opacity", 0);
+          });
 
       })
   );
